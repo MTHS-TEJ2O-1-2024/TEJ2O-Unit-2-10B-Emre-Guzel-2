c3f6e53413f8a187f6593701c44399edc76d077b
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-18 23:40:00 -0400

    added light sansor

--- main.ts
+++ main.ts
@@ -7,17 +7,32 @@
 // Seting the screen
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
-// Settign my varibel and my code
-let lightLevel: neopixel.Strip = null
-// Setting the neopixel
-lightLevel = neopixel.create(DigitalPin.P16, 4, NeoPixelMode .RGB)
-lightLevel.setPixelColor(0,neopixel.colors(NeoPixelColors.Black))
-lightLevel.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-lightLevel.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-lightLevel.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-lightLevel.show()
+// Loop
+basic.forever(function () {
+    // Setting my varibel and my code
+    let lightLevel: number = null
+    // Setting the neopixel
+    lightLevel = input.lightLevel()
+    // Added ligths becasue we want to control the NeoPixel LEDs
+    let lights = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    if (lightLevel <= 51) {
+        lights.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        lights.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        lights.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        lights.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    }
+    if (lightLevel > 52) {
+        lights.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))    
+    }
+    if (lightLevel > 104) {
+        lights.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    }
+    if (lightLevel > 156) {
+        lights.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    }
+    if(lightLevel >208){
+        lights.setPixelColor(3, neopixel.colors(NeoPixelColors.Purple))
+    }
+    lights.show()
 
-if (lightLevel <= 51) {
-
-}
-
+})

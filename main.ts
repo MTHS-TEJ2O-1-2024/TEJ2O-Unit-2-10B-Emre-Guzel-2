c61de8542d3af732d0327d8a72d0846aa2c566d7
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-18 20:50:42 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Emre Guzel
+ * Created on: Oct 18 2024
+ * This program reads the anmount of light 
 */
+// Seting the screen
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+// Settign my varibel and my code
+let lightLevel: neopixel.Strip = null
+// Setting the neopixel
+lightLevel = neopixel.create(DigitalPin.P16, 4, NeoPixelMode .RGB)
+lightLevel.setPixelColor(0,neopixel.colors(NeoPixelColors.Black))
+lightLevel.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+lightLevel.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+lightLevel.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+lightLevel.show()
 
-basic.showString('Hello, World!')
+if (lightLevel <= 51) {
+
+}
+

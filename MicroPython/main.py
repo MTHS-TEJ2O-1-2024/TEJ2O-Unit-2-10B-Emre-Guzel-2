fc60820bca805bdcd72490f6ab58dd8d70a293b2
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-20 16:13:05 +0000

    finshed light mesure program

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -13,9 +13,9 @@ display.show(Image.HAPPY)
 # Loop
 while True:
     # Setting my veriblas for the program
-    light_Level = int
-    display.read_light_level()
-    lights = neopixel.NeoPixel(pin16, 3)
+    light_Level = 0
+    light_Level = display.read_light_level()
+    lights = neopixel.NeoPixel(pin16, 4)
 
     # Making the ligths off when it is smaller than 51
     if light_Level <= 51:
@@ -24,8 +24,17 @@ while True:
         lights[2] = (0, 0, 0)
         lights[3] = (0, 0, 0)
 
-# When it is bigger than 52 turns on one LED
+    # When it is bigger than 52 turns on 1 LED
+    if light_Level > 208:
+        lights[0] = (127, 255, 0)
+
+    if light_Level > 156:
+        lights[1] = (75, 0, 130)
+
+    if light_Level > 104:
+        lights[2] = (0, 191, 255)
+
     if light_Level > 52:
-        lights[0] = (122, 50, 40)
+        lights[3] = (0, 255, 255)
 
     lights.show()
